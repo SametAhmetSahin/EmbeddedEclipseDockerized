bcbacdbd06e0439adcdae4e204b643bfee6c14b0
Samet Ahmet Şahin <sametahmetsahin@protonmail.com>
2021-10-22 14:24:54 +0300

    wip

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,9 @@
 FROM ubuntu:focal
 WORKDIR /app
 COPY . /app
+#RUN echo "eclipsedockerized" > /etc/hostname
 RUN apt update
-RUN echo -e "8\n20" apt install -y --no-install-recommends libxcursor1 libxinerama1 libxrandr2 libxi6 libswt-gtk-4-jni openocd gcc-arm-none-eabi usbutils build-essential libcanberra-gtk3-0 gdb-multiarch
-
+RUN echo -e "8\n20" apt install -y --no-install-recommends libxcursor1 libxinerama1 libxrandr2 libxi6 libswt-gtk-4-jni openocd gcc-arm-none-eabi usbutils build-essential libcanberra-gtk3-0 gdb-multiarch ssh xterm
 # add new user
 
 RUN groupadd -r eclipse && useradd -r -s /bin/bash -g eclipse eclipse
@@ -11,7 +11,7 @@ RUN groupadd -r eclipse && useradd -r -s /bin/bash -g eclipse eclipse
 RUN chown -R eclipse:eclipse /app
 #RUN echo -e eclipse | passwd eclipse
 RUN chpasswd passwd.txt
-USER eclipse
+#USER eclipse
 
 # Extra packages for testing, will comment in production
 # RUN apt install -y --no-install-recommends xterm
--- run.sh
+++ run.sh
@@ -2,9 +2,9 @@
 # Samet Ahmet Şahin 11.10.2021
 echo "This script will run xhost and run the Embedded Eclipse Docker container and display Eclipse GUI\n"
 echo "The projects folder and the configuration (todo) folder will be at /home/usernamehere/EmbeddedEclipse"
-xhost +
+#xhost +
 mkdir /home/$USER/EmbeddedEclipse/
 mkdir /home/$USER/EmbeddedEclipse/projects/
 sudo chown -hR $USER:$USER /home/$USER/EmbeddedEclipse
-sudo docker run --rm -it -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY --device /dev/bus/usb/002 -v "/home/$USER/EmbeddedEclipse/projects/:/apps/projects/:rw" embeddedeclipsedockerizeduservariant /bin/bash #/app/eclipse/eclipse
+sudo docker run --rm -it -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY --device /dev/bus/usb/002 -v "/home/$USER/EmbeddedEclipse/projects/:/apps/projects/:rw" --hostname "eclipsedockerized" embeddedeclipsedockerizeduservariant xterm #/app/eclipse/eclipse
 #-v "/home/$USER/EmbeddedEclipse/configuration:/apps/eclipse/configuration/:rw"
